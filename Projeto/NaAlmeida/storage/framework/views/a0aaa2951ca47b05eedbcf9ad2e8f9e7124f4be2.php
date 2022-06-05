f017dac75dc5a58dc0341f64f5715be28212bbe2
naalmeida98 <natii_98_@hotmail.com>
2022-06-05 16:25:14 -0300

    atualização do sistema

--- Projeto/NaAlmeida/storage/framework/views/a0aaa2951ca47b05eedbcf9ad2e8f9e7124f4be2.php
+++
@@ -1,5 +0,0 @@
-<?php $__env->startSection('title', __('Not Found')); ?>
-<?php $__env->startSection('code', '404'); ?>
-<?php $__env->startSection('message', __('Not Found')); ?>
-
-<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Samsung\Documents\GitHub\2021-02-atividades-naalmeida98\Projeto\NaAlmeida\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions/views/404.blade.php ENDPATH**/ ?>
