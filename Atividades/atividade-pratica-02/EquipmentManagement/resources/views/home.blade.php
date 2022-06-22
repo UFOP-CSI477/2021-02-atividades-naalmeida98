09bf4ab596ec92e0dd3892a87f0ebfd479910b73
naalmeida98 <natii_98_@hotmail.com>
2022-06-21 21:28:04 -0300

    update

--- Atividades/atividade-pratica-02/EquipmentManagement/resources/views/home.blade.php
+++ Atividades/atividade-pratica-02/EquipmentManagement/resources/views/home.blade.php
@@ -10,6 +10,8 @@
     <img src="{{ asset('images/home.jpeg') }}" alt="home" width="100%">
 </div>
 
+<iframe src="https://app.vectary.com/p/7QDF5JjzqT1iEvYtOkQmdc" frameborder="0" width="100%" height="280"></iframe>
+
 <div class="d-flex flex-row justify-content-center">
   <div class="p-3" > <a href="{{ route('equipamentos.index') }}" class="btn btn-dark">Relatório de equipamentos</a> </div>
   <div class="p-3" > <a href="{{ route('manutencoes.index') }}" class="btn btn-dark">Relatório de manutenções</a> </div>
